6f81fc940fff071d7ba1d30e00aa953a71ead4a0
daniel.ebert <daniel.ebert@web.de>
2021-05-11 14:53:25 +0200

    installed inquirer

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,15 @@
-import fs from "fs";
+// const readline = require("readline").createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
 
-console.log(fs.readFileSync("./README.md", "utf-8"));
+// readline.question(`What's your password?`, (password: string) => {
+//   console.log(`Great, I know your password: ${password}!`);
+//   readline.question(
+//     `What would you like to do with the password?`,
+//     (action: string) => {
+//       console.log(`Ok, I will ${action}!`);
+//       readline.close();
+//     }
+//   );
+// });
