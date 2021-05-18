41216815700ab525ecc46dfd45adca6fdda47889
daniel.ebert <daniel.ebert@web.de>
2021-05-18 11:36:54 +0200

    Add mongodb module

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,7 +1,3 @@
-// import { MongoClient } from "mongodb";
-
-// mongodb+srv://Daniel:<password>@cluster0.yyjvp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
-
 export const connectDatabase = async (url: string): Promise<void> => {
   console.log(url);
 };
